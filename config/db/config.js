2fee304e4306c73d2c4e4c125a279cbff6dfd5d2
Taras Burdeniuk <bur.taras@gmail.com>
2019-04-05 21:03:13 +0300

    added tests for routes change route to data base deleted some 'console.log' in db.js

--- config/db/config.js
+++ config/db/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  uri: 'mongodb+srv://Master:BMiu0Lc7rEZYjYff@notes-jh74y.mongodb.net/test?retryWrites=true',
+  uri: 'mongodb+srv://taras:1234@basex-yb7bi.mongodb.net/test?retryWrites=true',
   dbName: 'todo'
 };
